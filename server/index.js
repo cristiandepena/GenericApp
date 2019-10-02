c5cf7dc2cbdfedd8b1a81f85aa4daffd3e72760c
Cristian De Peña <cristiandepena@hotmail.com>
2019-10-02 16:28:08 -0400

    Adding body-parser and post route

--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 8001;
 const dir = path.join(__dirname, '/../');
 const appDir = path.join(dir, '/app/');
 
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.use(bodyParser.json());
+
 app.use(express.static(appDir));
 
+// Routes
+app.post('/authorize', (req, res) => {
+  const { username, password } = req.body;
+  console.log(username);
+  res.send(`Hello ${username}! Your password is: ${password}.`);
+});
+
+// Always last
 app.get('*', (req, res) => {
   res.sendFile(`${appDir}index.html`);
 });
